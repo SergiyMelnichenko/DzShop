cb14ea8490905175336fdd5f60cfdbe031a0ecd8
SergiyMelnichenko <rekorl30@gmail.com>
2020-03-09 15:15:29 +0200

    Added model Items - Added model Items (title, summary, price) - New actions for Items (index, show, new, create, destroy) - Views for all Actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :items
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  get 'pages/index'
+  get 'items/index'
+  root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
